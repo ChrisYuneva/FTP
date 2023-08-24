f87ce257b6127194a9910a27fc3807a21238c66d
ChrisYuneva <christina_yuneva@mail.ru>
2023-08-24 17:19:43 +0400

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {Button} from "@mui/material";
+
+function App() {
+    return (
+        <div className="App">
+            test
+            <Button variant="contained">Hello world</Button>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { store } from './store/store';
+import {Provider} from 'react-redux';
+
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <div>Sorry, page not found</div>,
+    },
+]);
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <CssBaseline />
+            <RouterProvider router={router}/>
+        </Provider>
+    </React.StrictMode>
+);
